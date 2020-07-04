90560609d237850ccde86c3641b01a4025169abf
Marcin <marcinkaim96@gmail.com>
2020-07-04 20:51:18 +0200

    Add semicolons at the and of the lines

--- BetterCalendar/wwwroot/js/main.js
+++ BetterCalendar/wwwroot/js/main.js
@@ -11,56 +11,56 @@
     methods: {
 
         openModal: function () {
-            this.modalOpened = true
-            this.errors = []
+            this.modalOpened = true;
+            this.errors = [];
         },
 
         closeModal: function (event) {
-            if (event.target.id == 'overlay' || event.target.id == 'close-modal-button') {
-                this.modalOpened = false
+            if (event.target.id === 'overlay' || event.target.id === 'close-modal-button') {
+                this.modalOpened = false;
             }
         },
 
         checkForm: function () {
-            this.errors = []
+            this.errors = [];
 
-            if (this.title.length == 0) {
-                this.errors.push("Tytuł jest wymagany")
+            if (this.title.length === 0) {
+                this.errors.push("Tytuł jest wymagany");
             }
 
-            if (this.validateHours() == false) {
-                this.errors.push("Godzina rozpoczęcia nie moze byc pózniejsza niż godzina zakończenia")
+            if (this.validateHours() === false) {
+                this.errors.push("Godzina rozpoczęcia nie moze byc pózniejsza niż godzina zakończenia");
             }
 
-            if (this.errors.length == 0) {
-                document.querySelector("#newEventForm").submit()
+            if (this.errors.length === 0) {
+                document.querySelector("#newEventForm").submit();
             }
 
-            
+
         },
 
         validateHours: function () {
 
-            if (this.end.length == 0) {
-                return true
+            if (this.end.length === 0) {
+                return true;
             }
 
-            let startArr = this.start.split(":")
-            let endArr = this.end.split(":")
+            let startArr = this.start.split(":");
+            let endArr = this.end.split(":");
 
             if (parseInt(startArr[0]) > parseInt(endArr[0])) {
-                return false
+                return false;
             }
 
-            if (parseInt(startArr[0]) == parseInt(endArr[0])) {
+            if (parseInt(startArr[0]) === parseInt(endArr[0])) {
                 if (parseInt(startArr[1]) > parseInt(endArr[1])) {
-                    return false
+                    return false;
                 }
 
-                return true
+                return true;
             }
 
-            return true
+            return true;
         }
-    },
-})
+    }
+});
--- BetterCalendar/wwwroot/js/menu.js
+++ BetterCalendar/wwwroot/js/menu.js
@@ -1,29 +1,25 @@
-﻿var closeButton = document.querySelector("#close-menu-button")
-var menuItems = document.querySelector("#menu-items")
-var openButton = document.querySelector(".menu")
-var menuOverlay = document.querySelector(".menu-overlay")
+﻿var closeButton = document.querySelector("#close-menu-button");
+var menuItems = document.querySelector("#menu-items");
+var openButton = document.querySelector(".menu");
+var menuOverlay = document.querySelector(".menu-overlay");
 
 
 
 
 openButton.addEventListener("click", function (e) {
-    menuOverlay.classList.remove("hide")
-    menuOverlay.classList.add("show")
-    menuItems.classList.remove("hide")
-    menuItems.classList.add("show")
-})
+    menuOverlay.classList.remove("hide");
+    menuOverlay.classList.add("show");
+    menuItems.classList.remove("hide");
+    menuItems.classList.add("show");
+});
+    
+closeButton.addEventListener("click", closeMenu());
 
-closeButton.addEventListener("click", function (e) {
-    menuOverlay.classList.remove("show")
-    menuOverlay.classList.add("hide")
-    menuItems.classList.remove("show")
-    menuItems.classList.add("hide")
+menuOverlay.addEventListener("click", closeMenu());
 
-})
-
-menuOverlay.addEventListener("click", function (e) {
-    menuOverlay.classList.remove("show")
-    menuOverlay.classList.add("hide")
-    menuItems.classList.remove("show")
-    menuItems.classList.add("hide")
-})
+function closeMenu() {
+    menuOverlay.classList.remove("show");
+    menuOverlay.classList.add("hide");
+    menuItems.classList.remove("show");
+    menuItems.classList.add("hide");
+}
